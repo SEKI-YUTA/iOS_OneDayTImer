dcf704f57145c61c52a7e07a8f92445e391c4b38
SEKI-YUTA <zard.since1991@gmail.com>
2024-03-04 01:08:14 +0900

    Initial Commit

---
+++ OneDayTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneDayTimer
+//
+//  Created by 関佑太 on 2024/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneDayTimer/OneDayTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneDayTimerApp.swift
+//  OneDayTimer
+//
+//  Created by 関佑太 on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct OneDayTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
